ecea48625ad1ae6723cdedeaa61dcd00908a3af3
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-10-19 10:10:00 +0300

    Added OrderItems class

---
+++ src/main/java/com/krekerok/onlinestore/entities/OrderItems.java
@@ -0,0 +1,39 @@
+package com.krekerok.onlinestore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class OrderItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public OrderItems(Order order, Product product, int quantity) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+    }
+}
