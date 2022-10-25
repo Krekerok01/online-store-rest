766d2730228f91ebb31cb289fe136979abb6cb60
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-10-25 19:15:02 +0300

    Refactoring changes

--- src/main/java/com/krekerok/onlinestore/services/ProductServiceImpl.java
+++ src/main/java/com/krekerok/onlinestore/services/ProductServiceImpl.java
@@ -9,7 +9,6 @@ import com.krekerok.onlinestore.repositories.ProductRepository;
 import com.krekerok.onlinestore.services.interfaces.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -39,6 +38,17 @@ public class ProductServiceImpl implements ProductService {
         return  ResponseEntity.ok(new ProductResponse(id, product.getName(), product.getPrice(), product.getStatus(), product.getCreatedAt()));
     }
 
+    @Override
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        List<ProductResponse> products = productRepository.findAll()
+                .stream()
+                .map((product ->
+                        new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getStatus(), product.getCreatedAt())))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(products);
+    }
 
 
     @Override
@@ -73,18 +83,6 @@ public class ProductServiceImpl implements ProductService {
 
         productRepository.deleteById(id);
         return ResponseEntity.ok(new MessageResponse("Product DELETED"));
-    }
-
-    @Override
-    public ResponseEntity<List<ProductResponse>> getAllProducts() {
-
-        List<ProductResponse> products = productRepository.findAll()
-                .stream()
-                .map((product ->
-                        new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getStatus(), product.getCreatedAt())))
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(products);
     }
 
 }
